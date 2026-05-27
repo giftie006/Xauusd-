827a52dd7992fa4d044cbed9bdf07456bbf7e0fd
giftie006 <gngwatu007@gmail.com>
2026-05-27 07:04:29 +0300

    Create Giftiebot.py

---
+++ Giftiebot.py
@@ -0,0 +1,10 @@
+import telebot
+
+TOKEN = "8975498976:AAFzmPtc3SJGWusLEZIkiL7x3D8lThkq6YM"
+CHAT_ID = "6813588628"
+
+bot = telebot.TeleBot(8975498976:AAFzmPtc3SJGWusLEZIkiL7x3D8lThkq6YM)
+
+bot.send_message(CHAT_ID, "🔥 XAUUSD bot connected successfully")
+
+print("Bot running...")
